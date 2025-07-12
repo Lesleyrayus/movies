8b2b645ff949600d7b23839738cf6e6289e592f0
Lesleyrayus <santhirim593@gmail.com>
2025-07-13 00:50:40 +0630

    change

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'react-firebase-course-main',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'react-firebase-course-main',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
